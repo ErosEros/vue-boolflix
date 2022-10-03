6300f6210acb9ee0c9b3f5a0a947232ed3baca2b
ErosEros <eros.vittori1994@gmail.com>
2022-10-04 00:05:49 +0200

    add search button

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    Ciao
+  <div>
+    <input type="text" v-model="query"/> <button @click="search" >CERCA</button>
+  </div>
   </div>
 </template>
 
@@ -11,10 +13,19 @@ import { apiKey } from './components/env';
 
 export default {
   name: 'App',
-  mounted(){
-    this.queryApi('ritorno al fut');
+  data(){
+    return {
+      query: ''
+    }
   },
+  // mounted(){
+  //   this.queryApi('ritorno al fut');
+  // },
   methods: {
+    search(){
+      this.queryApi(this.query);
+    },
+
     queryApi(textToSearch){
     axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${textToSearch}&language=it-IT`)
     .then((response)=>{
