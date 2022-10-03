fdab61f48d9dd12d9084c0e120f8b1f76dc64066
ErosEros <eros.vittori1994@gmail.com>
2022-10-03 23:35:44 +0200

    print in console my search

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,37 @@
 <template>
   <div id="app">
+    Ciao
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+import { apiKey } from './components/env';
 
 
 export default {
   name: 'App',
-  components: {
+  mounted(){
+    this.queryApi('ritorno al fut');
+  },
+  methods: {
+    queryApi(textToSearch){
+    axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${textToSearch}&language=it-IT`)
+    .then((response)=>{
+      console.log(response);
+    })
+    .catch(error =>{
+      console.log(error.message)
+    })
+  }
 
   }
+
+
 }
 </script>
 
 <style lang="scss">
+  @import '~bootstrap/scss/bootstrap';
 
 </style>
